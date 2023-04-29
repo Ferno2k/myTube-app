9ff718763012af9e849517025665d35741817617
Ferno2k <109463444+Ferno2k@users.noreply.github.com>
2023-04-29 15:01:37 +0530

    fixing repository 2

---
+++ src/components/Videos.jsx
@@ -0,0 +1,29 @@
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
+
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return 'Loading...';
+
+  return (
+    <Stack
+      direction={direction || 'row'}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => {
+        console.log(item);
+        if (item.id.videoId || item.id.channelId) {
+          return (
+            <Box key={idx}>
+              {item.id.videoId && <VideoCard video={item} />}
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+          );
+        }
+        return '';
+      })}
+    </Stack>
+  );
+};
+export default Videos;
